1ebae6891886d7374382d609b6b5b2316967eb14
ddimitrovd <ddimitrovd@abv.bg>
2017-10-13 18:00:17 -0400

    devoir 2 avant de unifier les types de calcul dacceleration

--- Devoir 2/src/Devoir2.m
+++ Devoir 2/src/Devoir2.m
@@ -1,11 +1,12 @@
-function [coup tf rbf vbf] = Devoir2(option, rbi, vbi, wbi)
+function [coup tf vbf x y z] = Devoir2(option, rbi, vbi, wbi)
   
-  accGrav = [0, 0, -9.8];
-  dt = 0.05;
+  accGrav = [0, 0, -9.8]';
+  dt = 0.0002;
   T = 0;
   vit = vbi;
   pos = rbi;
   m = 0.0027;
+  r = 0.0199;
   
   % Option 1 force grav seulement (force en Z)
   if (option == 1)
@@ -17,7 +18,7 @@ function [coup tf rbf vbf] = Devoir2(option, rbi, vbi, wbi)
     z=[];
     t=[];
     
-    while (Condition(pos) == -1)
+    while (VerifierConditions(pos) == -1) 
        pos = CalcPos(acc, vbi, rbi, T);
        t = [t T];
        x = [x pos(1)];
@@ -28,7 +29,6 @@ function [coup tf rbf vbf] = Devoir2(option, rbi, vbi, wbi)
     
     % Retourner les resultats
     tf = T;
-    rbf = [x, y, z];
     vbf = CalcVit(acc,vbi,tf);
     coup = 1; %*********************************************************
      
@@ -39,12 +39,12 @@ function [coup tf rbf vbf] = Devoir2(option, rbi, vbi, wbi)
     z=[pos(3)];
     t=[T];
     
-    while (Condition(pos) == -1)    
+    while (VerifierConditions(pos) == -1)    
       % Nouvelle iteration dependement de forces en jeu
       if (option == 2)
-        vit = RungeKutta (pos, wbi, dt, m , @Acc2);
+        vit = RungeKutta (vit, wbi, dt, m, r, @Acc2);
       else
-        vit = RungeKutta (pos, wbi, dt, m , @Acc3);
+        vit = RungeKutta (vit, wbi, dt, m, r, @Acc3);
       endif
       
       % Mettre a jour la position     
@@ -58,21 +58,71 @@ function [coup tf rbf vbf] = Devoir2(option, rbi, vbi, wbi)
     end
     
     tf = T;
-    rbf = pos
     vbf = vit
     coup = 1; %*********************************************************
   endif
 endfunction
 
 
-% Chercher la position suivate de la balle avec RungeKutta
-function qs = RungeKutta ( v, w, dt, m, fnc ) 
-  k1 = feval ( fnc , v , w, m )* dt ;
-  k2 = feval ( fnc , v + k1 /2 , w, m)* dt ;
-  k3 = feval ( fnc , v + k2 /2 , w, m)* dt ;
-  k4 = feval ( fnc , v + k3 , w, m)* dt ;
+% Chercher la nouvelle vitesse de la balle avec RungeKutta
+function qs = RungeKutta ( v, w, dt, m, r, fnc ) 
+  k1 = feval ( fnc , v , w, m, r )* dt ;
+  k2 = feval ( fnc , v + k1 /2 , w, m, r)* dt ;
+  k3 = feval ( fnc , v + k2 /2 , w, m, r)* dt ;
+  k4 = feval ( fnc , v + k3 , w, m, r)* dt ;
   qs = v +( k1 +2* k2 +2* k3 + k4 )/6;
 endfunction
+
+
+% Acceleration on option Grav
+function [ accel ] = Acc1(v, w, m, r)
+    grav = [0,0,-9.8]';  
+    accel = grav;
+endfunction
+
+
+% Acceleration on option Grav + Visc
+function [ accel ] = Acc2(v, w, m, r)
+    grav = [0,0,-9.8]';   
+    friction = Friction(v)/m;
+    
+    accel = grav+friction;
+endfunction
+
+
+% Acceleration on option Grav + Visc + Magnus
+function [ accel ] = Acc3(v, w, m, r)
+    grav = [0,0,-9.8]';
+    magnus = Magnus(v,w,r)/m;
+    friction = Friction(v) /m;
+    
+    accel = grav+friction+magnus;
+endfunction
+
+
+function [ f ] = Friction(v)
+    l = norm(v);
+    p= 1.2;
+    Cv = 0.14;
+    A = pi*(21.35/1000)^2;
+    f = - (p*Cv*A/2)*l*v;
+endfunction
+
+
+function [ Fm ] = Magnus(v,w,r)
+    p = 1.2;
+    Cm = 0.000791*norm(w);
+    A = pi*(r)^2;
+    wCv = cross(w,v);
+    wCv = wCv/norm(wCv);
+    Fm = p*Cm*A*(norm(v)^2)/2*wCv;  
+endfunction
+
+
+
+
+
+
 
 
 
--- Devoir 2/src/Exec.m
+++ Devoir 2/src/Exec.m
@@ -1,9 +1,8 @@
-rbi1 = [0.00, 0.50, 1.10];
-vbi1 = [4.00, 0.00, 0.80];
-wbi1 = [0.00, -70.00, 0.00];
+rbi1 = [0.00, 0.50, 1.10]';
+vbi1 = [4.00, 0.00, 0.80]';
+wbi1 = [0.00, -70.00, 0.00]';
 
-[coup11 tf11 rbf11 vbf11] = Devoir2(1, rbi1, vbi1, wbi1);
-Plot(rbf11, [0,0,0], [0,0,0], 'Essai 1');
-%[coup12 tf12 rbf12 vbf12] = Devoir2(2, rbi1, vbi1, wbi1);
-%[coup13 tf13 rbf13 vbf13] = Devoir2(3, rbi1, vbi1, wbi1);
-%PlotGraph(rbf11, rbf12, rbf13, 'Essai 1');
+[coup11 tf11 vbf11 x11 y11 z11] = Devoir2(1, rbi1, vbi1, wbi1);
+[coup12 tf12 vbf12 x12 y12 z12] = Devoir2(2, rbi1, vbi1, wbi1);
+[coup13 tf13 vbf13 x13 y13 z13] = Devoir2(3, rbi1, vbi1, wbi1);
+Plot(x11, y11, z11, x12, y12, z12, x13, y13, z13, 'Essai 1');
