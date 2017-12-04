3cf43e23cde3554e56f32617099f8463d1bf7499
ddimitrovd <ddimitrovd@abv.bg>
2017-12-04 12:13:04 -0500

    plot

--- Devoir 4/src/Exec.m
+++ Devoir 4/src/Exec.m
@@ -2,4 +2,5 @@ nout = 1;
 nin = 1.5;
 poso = [0, 4, 11];
 
-[xi yi zi face] = Devoir4(nout, nin, poso)
+[xi yi zi face] = Devoir4(nout, nin, poso);
+Plot(xi, yi, zi, face);
--- Devoir 4/src/Plot.m
+++ Devoir 4/src/Plot.m
@@ -1,12 +1,25 @@
-function Plot()   
+function Plot(x, y, z, face)   
  
+    n = columns(x);
+    
     figure('Name', "Figure") ;
+    
+    for i = 2:n
+      scatter3(x(i) ,y(i), z(i), face(i), "filled");
+       hold on;
+    endfor
+    
     [x, y, z] = cylinder ([2 2], 30);
-    mesh (x, y, z);
+    for i = 2:19
+      mesh (x + 4, y + 4, z+i, "facecolor", "none", "edgecolor", "black");
+      hold on;
+    endfor
     
     title("Figure");
-    %xlim([-5 105]);
-    %ylim([-5 105]);
+    xlim([0 8]);
+    ylim([0 8]);
+    zlim([0 22]);
     xlabel('X');
     ylabel('Y');
+    zlabel('Z');
 end
